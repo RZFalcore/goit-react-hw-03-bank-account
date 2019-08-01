2360f3863422293d1e9b9eb2665e1624ee7b093c
RZFalcore <box.rz.box@gmail.com>
2019-08-01 17:16:28 +0300

    Updates

---
+++ precache-manifest.c462b9bb2bd54b95f331cb1c4ac7ec57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f636c2d7a3c506ebe98aeda5b8d567da",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "4550c8f95eb598ebce77",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "4cccb552a9be86659ba2",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.a996c859.chunk.css"
+  },
+  {
+    "revision": "4550c8f95eb598ebce77",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.6b87b0f9.chunk.js"
+  },
+  {
+    "revision": "4cccb552a9be86659ba2",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.0425acc7.chunk.js"
+  },
+  {
+    "revision": "1d8f9ed0b38c93c66413",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime~main.d734538f.js"
+  }
+]);
